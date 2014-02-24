a4e47a1e3c8c04bf7750eac0a263b702cbc63ad3
Rob Charlwood <rob.charlwood@maplecroft.com>
2014-02-24 14:04:33 +0000

    Fixed other redirect issues

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -89,7 +89,7 @@ angular.module('BadMovieKnights.controllers', [])
 
     // method to cancel the editing of a blog entry
     $scope.cancelEdit = function() {
-      $location.path('/entry/' + entry.id);
+      $location.path('/');
     };
 
     // method to handle entry delete
@@ -139,7 +139,7 @@ angular.module('BadMovieKnights.controllers', [])
       // attempt the update
       EntryService.update($scope.entry).then(
         function(data){
-            $location.path('/entry/' + entry.id);
+            $location.path('/');
         },
         function(data){
           if (data.title) {
